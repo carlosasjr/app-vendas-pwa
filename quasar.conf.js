a179536e39abb73e0b0cff95fa1eb511af8c15b8
carlos <carlosasjr2003@hotmail.com>
2022-02-23 09:22:42 -0300

    new icons png

--- quasar.conf.js
+++ quasar.conf.js
@@ -131,27 +131,22 @@ module.exports = function (ctx) {
         categories: ["business", "tools", "productivity", "finance"],
         icons: [
           {
-            src: "icons/icon-128x128.png",
+            src: "icons/128.png",
             sizes: "128x128",
             type: "image/png",
           },
           {
-            src: "icons/icon-192x192.png",
+            src: "icons/192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "icons/icon-256x256.png",
-            sizes: "256x256",
-            type: "image/png",
-          },
-          {
-            src: "icons/icon-384x384.png",
+            src: "icons/384.png",
             sizes: "384x384",
             type: "image/png",
           },
           {
-            src: "icons/icon-512x512.png",
+            src: "icons/512.png",
             sizes: "512x512",
             type: "image/png",
           },
